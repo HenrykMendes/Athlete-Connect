72bea6c69892886adfff16d4d6d6bcf43ea19010
Pedro Silva <pedro.o.silva975@gmail.com>
2024-11-02 17:45:20 -0300

    build: homepage post block, postButton

---
+++ Athlete_Connect/01 - telas principais/assets/index.js
@@ -0,0 +1,25 @@
+let modal = document.querySelector(".modal");
+let btn = document.querySelector(".postButton");
+let span = document.querySelector(".close");
+let btnModal = document.querySelector(".modal button");
+
+// function para abrir o modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// function para fechar o modal ao verificar que o input está preenchido
+btnModal.onclick = function () {
+  if (document.querySelector(".modal input").value === "") {
+    alert("Preencha o campo");
+  } else {
+    modal.style.display = "none";
+  }
+};
+
+// function para fechar o modal ao clicar no botão de fechar
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+
