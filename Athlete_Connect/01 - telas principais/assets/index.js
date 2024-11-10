d7e87cf8bb8be4012fcea27632791258e8ff48a7
Pedro Silva <pedro.o.silva975@gmail.com>
2024-11-10 14:16:43 -0300

    build: novo botão de like adicionado

--- Athlete_Connect/01 - telas principais/assets/index.js
+++ Athlete_Connect/01 - telas principais/assets/index.js
@@ -23,3 +23,19 @@ span.onclick = function () {
 };
 
 
+//botão de like
+function Toggle1(btn) {
+  var icon = btn.querySelector('i');
+
+  // Alternar entre os estados de coração preenchido e vazio
+  if (icon.classList.contains('bi-heart-fill')) {
+    icon.classList.remove('bi-heart-fill');
+    icon.classList.add('bi-heart');
+    btn.style.color = "grey"; // Cor cinza quando o coração não estiver preenchido
+  } else {
+    icon.classList.remove('bi-heart');
+    icon.classList.add('bi-heart-fill');
+    btn.style.color = "red"; // Cor vermelha quando o coração estiver preenchido
+  }
+}
+
